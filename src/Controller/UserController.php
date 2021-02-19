12b290d67b4f76f06d407b16e742c5c5ceeccb6a
Germain Grumel <germaingrumel@outlook.fr>
2021-02-19 15:33:34 +0100

    kevin l'animal

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,32 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use GuzzleHttp\Client;
+use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends AbstractController
 {
     /**
      * @Route("/user", name="user")
      */
-    public function index()
+    public function index($request)
     {
-        // $client = new Client();
-        // $response = $client->request('GET', 'https://api.github.com/repos/guzzle/guzzle');
+        $data = ["falcon", "sky", "cloud", "orange", "wood", "forest"];
 
-        // echo $response->getStatusCode(); // 200
-        // echo $response->getHeaderLine('content-type'); // 'application/json; charset=utf8'
-        // echo $response->getBody(); // '{"id": 1420053, "name": "guzzle", ...}'
-
-        // // Send an asynchronous request.
-        // $request = new \GuzzleHttp\Psr7\Request('GET', 'http://httpbin.org');
-        // $promise = $client->sendAsync($request)->then(function ($response) {
-        //     echo 'I completed! ' . $response->getBody();
-        // });
-
-        // $promise->wait();
-        // return $this->render('user/index.html.twig', [
-        //     'controller_name' => 'UserController',
-        // ]);
-        return json_encode(['call' => "hello world"]);
+        header('Content-type:application/json;charset=utf-8');
+        echo json_encode($data[0]);
     }
 }
