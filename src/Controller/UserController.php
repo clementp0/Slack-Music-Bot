f2a0df131054a3a3a94e2273c6bcb3850669e695
airdox <kevcanico25@gmail.com>
2021-02-19 16:23:31 +0100

    test request slack api

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,14 +5,31 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class UserController extends AbstractController
 {
+
+    private $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
     /**
      * @Route("/user", name="user")
      */
     public function index()
     {
+
+        $response = $this->client->request(
+            'GET',
+            'https://slack.com/api/METHOD_FAMILY.method'
+        );
+
+        dd($response);
+
         return new JsonResponse('test');
     }
 }
