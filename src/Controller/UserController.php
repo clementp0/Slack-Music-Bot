87eeb5b9008ee1c1bfeb2f521894276320201084
airdox <kevcanico25@gmail.com>
2021-02-19 16:46:43 +0100

    test query

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,31 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends AbstractController
 {
 
-    private $client;
-
-    public function __construct(HttpClientInterface $client)
-    {
-        $this->client = $client;
-    }
-
     /**
      * @Route("/user", name="user")
      */
-    public function index()
+    public function index(Request $request)
     {
-
-        $response = $this->client->request(
-            'GET',
-            'https://slack.com/api/METHOD_FAMILY.method'
-        );
-
-        dd($response);
-
-        return new JsonResponse('test');
+        return new JsonResponse($request->query->get('user_name'));
     }
 }
