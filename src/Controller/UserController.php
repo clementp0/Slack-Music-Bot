139a22613fe5de385a8e69013c88a994acdcae03
Germain Grumel <germaingrumel@outlook.fr>
2021-02-19 15:59:47 +0100

    updated userControler

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,17 +4,14 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Validator\Constraints\Json;
 
 class UserController extends AbstractController
 {
     /**
      * @Route("/user", name="user")
      */
-    public function index($request)
+    public function index()
     {
         return new JsonResponse('test');
     }
