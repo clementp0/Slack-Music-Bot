45a6820ca8ebae91fc992b8db7515f5fd9a15a4c
Camille Grumel <camillegrumel@MacBook-Pro-de-Camille-2.local>
2021-03-04 15:23:35 +0100

    updated UserController

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,20 +1,22 @@
 <?php
-
 namespace App\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
-
 class UserController extends AbstractController
 {
-
     /**
      * @Route("/user", name="user")
      */
     public function index(Request $request)
     {
-        return new JsonResponse(utf8_decode("<@".$request->request->get('user_id').">"));
+        $message = "Hey <@".$request->request->get('user_id').">, thanks for submitting your report!";
+        $struct = [
+            "blocks" => 
+            [
+                [
+                    "type" => "section", "text" => ["type" => "mrkdwn", "text" => $message]]]];
+        return new JsonResponse($struct);
     }
 }
